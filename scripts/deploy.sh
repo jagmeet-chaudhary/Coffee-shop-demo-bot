e46aeb661d95f9d62505efcf0273b75a0be61e94
Jagmeet chaudhary <jagmeet_chaudhary@live.com>
2021-10-01 12:42:23 +0530

    modifed deployment scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-echo "Copying build to Storage"
+echo "Copying zip file to storage"
 gsutil defacl ch -u AllUsers:READER gs://$STORAGE_BUCKET
-gsutil rsync -R /tmp/workspace/dist gs://$STORAGE_BUCKET
-gsutil setmeta -h "Cache-Control:private, max-age=0, no-transform" \
-  gs://$STORAGE_BUCKET/*.js
+gsutil cp  /tmp/workspace/Coffee-Shop.zip gs://$STORAGE_BUCKET
